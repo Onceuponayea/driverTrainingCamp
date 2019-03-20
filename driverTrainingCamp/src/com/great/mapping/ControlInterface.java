aecb82cf001908794e49713f973b4b61d8fcd52e
Administrator <Administrator@PCOS-1711041609>
2019-03-20 10:25:18 +0800

    把gbk改成utf-8

--- driverTrainingCamp/src/com/great/mapping/ControlInterface.java
+++ driverTrainingCamp/src/com/great/mapping/ControlInterface.java
@@ -8,32 +8,34 @@ import org.springframework.stereotype.Repository;
 import com.great.entity.Car;
 import com.great.entity.Message;
 import com.great.entity.School;
+import com.great.entity.SchoolState;
 import com.great.entity.Student;
+import com.great.entity.Teacher;
 
 @Repository
 public interface ControlInterface {
-	//�鿴���м�У��Ϣ
+	//查询所有学校
 	public List<School> selectAllSchool();
-	//ͨ����Уid�޸ļ�У״̬��Խ�ֹ�ͻָ����������
+	//ͨ通过学校id更新学校状态
 	public void updateSchoolStateById();
-	//�鿴���н�������ϸ��Ϣ
-	public void selectAllTeacher();
-	//ͨ���������������id
+	//查询所有老师集合
+	public List<Teacher> selectAllTeacher();
+	//ͨ通过老师姓名查老师
 	public void selectTeacherIdByName(@Param("teacher_name")String teacher_name );
-	//��ѯ���н�������Ϣ
+	//查询所有教练车
 	public List<Car> selectAllCar();
-	//�鿴����ѧԱ��Ϣ
+	//查询所有学生
 	public List<Student> selectAllStudent();
-	//��ѯ������Ϣ
+	//查询所有公告信息
 	public List<Message> selectAllMessage();
-	//���ӹ�����Ϣ
+	//新增公告信息
 	public void addMassage(@Param("massage_content")String massage_content ,
 			@Param("massage_time")String massage_time);
-	//��ѯ��У��״̬��������ʾ
-/*	public List<SchooleState> selectSchoolState();
-	//��ѯ��У��ͨ�����
-	public List<SchoolApply> selectSchoolApply();*/
-	//������У��ͨ����
+	//查询学校状态集合
+	public List<SchoolState> selectSchoolState();
+	//查询驾校申请表
+	public List<SchoolApply> selectSchoolApply();
+	//更新申请表
 	public void updateSchoolApplyState(@Param("schoolapply_id") int schoolapply_id);
 	
 	
