3e41f11bdfe8b736257add5a81a4f87ba7a7334f
lenovo <lenovo@DESKTOP-MHN1K2V>
2019-03-21 14:26:24 +0800

    评分实体类新增项目字段参数

--- driverTrainingCamp/src/com/great/entity/EvaTheacher.java
+++ driverTrainingCamp/src/com/great/entity/EvaTheacher.java
@@ -4,50 +4,72 @@ public class EvaTheacher {
 	private int evaluate_theacher_id;
 	private int theacher_id;
 	private int evaluate_id;
+	private int level_id;
 	private String evaluate_type;
 	private String evaluate_theacher_content;
 	private String evaluate_theacher_date;
+
 	public EvaTheacher() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public EvaTheacher(int evaluate_theacher_id, int theacher_id, int evaluate_id, String evaluate_type,
+
+	public EvaTheacher(int evaluate_theacher_id, int theacher_id, int evaluate_id, int level_id, String evaluate_type,
 			String evaluate_theacher_content, String evaluate_theacher_date) {
 		super();
 		this.evaluate_theacher_id = evaluate_theacher_id;
 		this.theacher_id = theacher_id;
 		this.evaluate_id = evaluate_id;
+		this.level_id = level_id;
 		this.evaluate_type = evaluate_type;
 		this.evaluate_theacher_content = evaluate_theacher_content;
 		this.evaluate_theacher_date = evaluate_theacher_date;
 	}
+
 	public int getEvaluate_theacher_id() {
 		return evaluate_theacher_id;
 	}
+
 	public void setEvaluate_theacher_id(int evaluate_theacher_id) {
 		this.evaluate_theacher_id = evaluate_theacher_id;
 	}
+
 	public int getTheacher_id() {
 		return theacher_id;
 	}
+
 	public void setTheacher_id(int theacher_id) {
 		this.theacher_id = theacher_id;
 	}
+
 	public int getEvaluate_id() {
 		return evaluate_id;
 	}
+
 	public void setEvaluate_id(int evaluate_id) {
 		this.evaluate_id = evaluate_id;
 	}
+
+	public int getLevel_id() {
+		return level_id;
+	}
+
+	public void setLevel_id(int level_id) {
+		this.level_id = level_id;
+	}
+
 	public String getEvaluate_type() {
 		return evaluate_type;
 	}
+
 	public void setEvaluate_type(String evaluate_type) {
 		this.evaluate_type = evaluate_type;
 	}
+
 	public String getEvaluate_theacher_content() {
 		return evaluate_theacher_content;
 	}
+
 	public void setEvaluate_theacher_content(String evaluate_theacher_content) {
 		this.evaluate_theacher_content = evaluate_theacher_content;
 	}
@@ -55,8 +77,9 @@ public class EvaTheacher {
 	public String getEvaluate_theacher_date() {
 		return evaluate_theacher_date;
 	}
+
 	public void setEvaluate_theacher_date(String evaluate_theacher_date) {
 		this.evaluate_theacher_date = evaluate_theacher_date;
 	}
-	
+
 }
